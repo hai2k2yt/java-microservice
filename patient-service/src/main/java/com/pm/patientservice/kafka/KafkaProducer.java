2fb8d2e02fcbc5c3f51f0a66aab03a6a4aa8526a
haint <haithachxa2002@gmail.com>
2025-04-09 16:14:03 +0700

    init

---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,36 @@
+package com.pm.patientservice.kafka;
+
+import com.pm.patientservice.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaProducer {
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        try {
+            byte[] eventBytes = event.toByteArray();
+            log.info("Message size: {} bytes", eventBytes.length);
+            kafkaTemplate.send("patient", eventBytes);
+        } catch (Exception e) {
+            log.error("Error when sending PatientCreated event: {}", event);
+            log.error(e.getMessage());
+        }
+    }
+}
